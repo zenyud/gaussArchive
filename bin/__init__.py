449cc611b3d9af214617aa8c17e2b61bd2757c0b
zenyud <56981833@qq.com>
2020-07-14 14:48:10 +0800

    Initial commit

---
+++ bin/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+# Date Time     : 2020/7/14 
+# Write By      : adtec(zengyu)
+# Function Desc :
+#             
+# History       :
+#                 2020/7/14  zengyu     Create
+#
+# Remarks       :
